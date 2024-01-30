1fdcf689f01395ffa200c6b88af7100bf206183b
enver-isliamov <enverphoto@gmail.com>
2024-01-30 11:55:23 +0300

    script.js

--- script.js
+++ script.js
@@ -1,140 +1,238 @@
-var app = new Vue({
-	el: "#app",
-	data: {
-		audio: "",
-		imgLoaded: false,
-		currentlyPlaying: false,
-		currentlyStopped: false,
-		currentTime: 0,
-		checkingCurrentPositionInTrack: "",
-		trackDuration: 0,
-		currentProgressBar: 0,
-		isPlaylistActive: false,
-		currentSong: 0,
-		debug: false,
-		musicPlaylist: [
-        {
-          title: "created by Enver Isliamov",
-          artist: "Canli Radio",
-          image: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/6.jpg",
-          url: "https://c28.radioboss.fm:18047/stream",
-        },
-		],
-		audioFile: ""
-	},
-	mounted: function() {
-		this.changeSong();
-		this.audio.loop = false;
-	},
-	filters: {
-		fancyTimeFormat: function(s) {
-			return (s - (s %= 60)) / 60 + (9 < s ? ":" : ":0") + s;
-		}
-	},
-	methods: {
-		togglePlaylist: function() {
-			this.isPlaylistActive = !this.isPlaylistActive;
-		},
-		nextSong: function() {
-			if (this.currentSong < this.musicPlaylist.length - 1)
-				this.changeSong(this.currentSong + 1);
-		},
-		prevSong: function() {
-			if (this.currentSong > 0) this.changeSong(this.currentSong - 1);
-		},
-		changeSong: function(index) {
-			var wasPlaying = this.currentlyPlaying;
-			this.imageLoaded = false;
-			if (index !== undefined) {
-				this.stopAudio();
-				this.currentSong = index;
-			}
-			this.audioFile = this.musicPlaylist[this.currentSong].url;
-			this.audio = new Audio(this.audioFile);
-			var localThis = this;
-			this.audio.addEventListener("loadedmetadata", function() {
-				localThis.trackDuration = Math.round(this.duration);
-			});
-			this.audio.addEventListener("ended", this.handleEnded);
-			if (wasPlaying) {
-				this.playAudio();
-			}
-		},
-		isCurrentSong: function(index) {
-			if (this.currentSong == index) {
-				return true;
-			}
-			return false;
-		},
-		getCurrentSong: function(currentSong) {
-			return this.musicPlaylist[currentSong].url;
-		},
-		playAudio: function() {
-			if (
-				this.currentlyStopped == true &&
-				this.currentSong + 1 == this.musicPlaylist.length
-			) {
-				this.currentSong = 0;
-				this.changeSong();
-			}
-			if (!this.currentlyPlaying) {
-				this.getCurrentTimeEverySecond(true);
-				this.currentlyPlaying = true;
-				this.audio.play();
-			} else {
-				this.stopAudio();
-			}
-			this.currentlyStopped = false;
-		},
-		stopAudio: function() {
-			this.audio.pause();
-			this.currentlyPlaying = false;
-			this.pausedMusic();
-		},
-		handleEnded: function() {
-			if (this.currentSong + 1 == this.musicPlaylist.length) {
-				this.stopAudio();
-				this.currentlyPlaying = false;
-				this.currentlyStopped = true;
-			} else {
-				this.currentlyPlaying = false;
-				this.currentSong++;
-				this.changeSong();
-				this.playAudio();
-			}
-		},
-		onImageLoaded: function() {
-			this.imgLoaded = true;
-		},
-		getCurrentTimeEverySecond: function(startStop) {
-			var localThis = this;
-			this.checkingCurrentPositionInTrack = setTimeout(
-				function() {
-					localThis.currentTime = localThis.audio.currentTime;
-					localThis.currentProgressBar =
-						localThis.audio.currentTime / localThis.trackDuration * 100;
-					localThis.getCurrentTimeEverySecond(true);
-				}.bind(this),
-				1000
-			);
-		},
-		pausedMusic: function() {
-			clearTimeout(this.checkingCurrentPositionInTrack);
-		},
-		toggleDebug: function(){
-			this.debug=!this.debug;
-			document.body.classList.toggle('debug');
-		}
-	},
-	watch: {
-		currentTime: function() {
-			this.currentTime = Math.round(this.currentTime);
-		}
-	},
-	beforeDestroy: function() {
-		this.audio.removeEventListener("ended", this.handleEnded);
-		this.audio.removeEventListener("loadedmetadata", this.handleEnded);
+$(function () {
+  var playerTrack = $("#player-track"),
+    bgArtwork = $("#bg-artwork"),
+    bgArtworkUrl,
+    albumName = $("#album-name"),
+    trackName = $("#track-name"),
+    albumArt = $("#album-art"),
+    sArea = $("#s-area"),
+    seekBar = $("#seek-bar"),
+    trackTime = $("#track-time"),
+    insTime = $("#ins-time"),
+    sHover = $("#s-hover"),
+    playPauseButton = $("#play-pause-button"),
+    i = playPauseButton.find("i"),
+    tProgress = $("#current-time"),
+    tTime = $("#track-length"),
+    seekT,
+    seekLoc,
+    seekBarPos,
+    cM,
+    ctMinutes,
+    ctSeconds,
+    curMinutes,
+    curSeconds,
+    durMinutes,
+    durSeconds,
+    playProgress,
+    bTime,
+    nTime = 0,
+    buffInterval = null,
+    tFlag = false,
+    albums = [
+      "Canli Radio",
+    ],
+    trackNames = [
+      "by Enver Isliamov",
+    ],
+    albumArtworks = ["_1", "_2", "_3", "_4", "_5"],
+    trackUrl = [
+      "https://c28.radioboss.fm:18047/stream",
+    ],
+    playPreviousTrackButton = $("#play-previous"),
+    playNextTrackButton = $("#play-next"),
+    currIndex = -1;
 
-		clearTimeout(this.checkingCurrentPositionInTrack);
-	}
+  function playPause() {
+    setTimeout(function () {
+      if (audio.paused) {
+        playerTrack.addClass("active");
+        albumArt.addClass("active");
+        checkBuffering();
+        i.attr("class", "fas fa-pause");
+        audio.play();
+      } else {
+        playerTrack.removeClass("active");
+        albumArt.removeClass("active");
+        clearInterval(buffInterval);
+        albumArt.removeClass("buffering");
+        i.attr("class", "fas fa-play");
+        audio.pause();
+      }
+    }, 300);
+  }
+
+  function showHover(event) {
+    seekBarPos = sArea.offset();
+    seekT = event.clientX - seekBarPos.left;
+    seekLoc = audio.duration * (seekT / sArea.outerWidth());
+
+    sHover.width(seekT);
+
+    cM = seekLoc / 60;
+
+    ctMinutes = Math.floor(cM);
+    ctSeconds = Math.floor(seekLoc - ctMinutes * 60);
+
+    if (ctMinutes < 0 || ctSeconds < 0) return;
+
+    if (ctMinutes < 0 || ctSeconds < 0) return;
+
+    if (ctMinutes < 10) ctMinutes = "0" + ctMinutes;
+    if (ctSeconds < 10) ctSeconds = "0" + ctSeconds;
+
+    if (isNaN(ctMinutes) || isNaN(ctSeconds)) insTime.text("--:--");
+    else insTime.text(ctMinutes + ":" + ctSeconds);
+
+    insTime.css({ left: seekT, "margin-left": "-21px" }).fadeIn(0);
+  }
+
+  function hideHover() {
+    sHover.width(0);
+    insTime.text("00:00").css({ left: "0px", "margin-left": "0px" }).fadeOut(0);
+  }
+
+  function playFromClickedPos() {
+    audio.currentTime = seekLoc;
+    seekBar.width(seekT);
+    hideHover();
+  }
+
+  function updateCurrTime() {
+    nTime = new Date();
+    nTime = nTime.getTime();
+
+    if (!tFlag) {
+      tFlag = true;
+      trackTime.addClass("active");
+    }
+
+    curMinutes = Math.floor(audio.currentTime / 60);
+    curSeconds = Math.floor(audio.currentTime - curMinutes * 60);
+
+    durMinutes = Math.floor(audio.duration / 60);
+    durSeconds = Math.floor(audio.duration - durMinutes * 60);
+
+    playProgress = (audio.currentTime / audio.duration) * 100;
+
+    if (curMinutes < 10) curMinutes = "0" + curMinutes;
+    if (curSeconds < 10) curSeconds = "0" + curSeconds;
+
+    if (durMinutes < 10) durMinutes = "0" + durMinutes;
+    if (durSeconds < 10) durSeconds = "0" + durSeconds;
+
+    if (isNaN(curMinutes) || isNaN(curSeconds)) tProgress.text("00:00");
+    else tProgress.text(curMinutes + ":" + curSeconds);
+
+    if (isNaN(durMinutes) || isNaN(durSeconds)) tTime.text("00:00");
+    else tTime.text(durMinutes + ":" + durSeconds);
+
+    if (
+      isNaN(curMinutes) ||
+      isNaN(curSeconds) ||
+      isNaN(durMinutes) ||
+      isNaN(durSeconds)
+    )
+      trackTime.removeClass("active");
+    else trackTime.addClass("active");
+
+    seekBar.width(playProgress + "%");
+
+    if (playProgress == 100) {
+      i.attr("class", "fa fa-play");
+      seekBar.width(0);
+      tProgress.text("00:00");
+      albumArt.removeClass("buffering").removeClass("active");
+      clearInterval(buffInterval);
+    }
+  }
+
+  function checkBuffering() {
+    clearInterval(buffInterval);
+    buffInterval = setInterval(function () {
+      if (nTime == 0 || bTime - nTime > 1000) albumArt.addClass("buffering");
+      else albumArt.removeClass("buffering");
+
+      bTime = new Date();
+      bTime = bTime.getTime();
+    }, 100);
+  }
+
+  function selectTrack(flag) {
+    if (flag == 0 || flag == 1) ++currIndex;
+    else --currIndex;
+
+    if (currIndex > -1 && currIndex < albumArtworks.length) {
+      if (flag == 0) i.attr("class", "fa fa-play");
+      else {
+        albumArt.removeClass("buffering");
+        i.attr("class", "fa fa-pause");
+      }
+
+      seekBar.width(0);
+      trackTime.removeClass("active");
+      tProgress.text("00:00");
+      tTime.text("00:00");
+
+      currAlbum = albums[currIndex];
+      currTrackName = trackNames[currIndex];
+      currArtwork = albumArtworks[currIndex];
+
+      audio.src = trackUrl[currIndex];
+
+      nTime = 0;
+      bTime = new Date();
+      bTime = bTime.getTime();
+
+      if (flag != 0) {
+        audio.play();
+        playerTrack.addClass("active");
+        albumArt.addClass("active");
+
+        clearInterval(buffInterval);
+        checkBuffering();
+      }
+
+      albumName.text(currAlbum);
+      trackName.text(currTrackName);
+      albumArt.find("img.active").removeClass("active");
+      $("#" + currArtwork).addClass("active");
+
+      bgArtworkUrl = $("#" + currArtwork).attr("src");
+
+      bgArtwork.css({ "background-image": "url(" + bgArtworkUrl + ")" });
+    } else {
+      if (flag == 0 || flag == 1) --currIndex;
+      else ++currIndex;
+    }
+  }
+
+  function initPlayer() {
+    audio = new Audio();
+
+    selectTrack(0);
+
+    audio.loop = false;
+
+    playPauseButton.on("click", playPause);
+
+    sArea.mousemove(function (event) {
+      showHover(event);
+    });
+
+    sArea.mouseout(hideHover);
+
+    sArea.on("click", playFromClickedPos);
+
+    $(audio).on("timeupdate", updateCurrTime);
+
+    playPreviousTrackButton.on("click", function () {
+      selectTrack(-1);
+    });
+    playNextTrackButton.on("click", function () {
+      selectTrack(1);
+    });
+  }
+
+  initPlayer();
 });
